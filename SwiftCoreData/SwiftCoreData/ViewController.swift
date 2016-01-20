4572e53fa5e6e61cf2a0d31721e39c124cda3ef8
Jagaa <jagaa1103@gmail.com>
2016-01-20 15:02:35 +0100

    CoreData Store is done

--- SwiftCoreData/SwiftCoreData/ViewController.swift
+++ SwiftCoreData/SwiftCoreData/ViewController.swift
@@ -11,16 +11,66 @@ import CoreData
 
 class tableViewController: UITableViewController {
 
+    var listItems = [NSManagedObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Add, target: self, action: Selector("addItem"))
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return listItems.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell")! as UITableViewCell
+        
+        return cell
+    }
+    
+    func addItem(){
+        print("Add Button Clicked")
+        let confirmView = UIAlertController(title: "Confirm View", message: "Add your Item", preferredStyle: .Alert)
+        let confirmAction = UIAlertAction(title: "Add Item", style: UIAlertActionStyle.Default, handler: ({
+            (_) in
+            
+                if let field = confirmView.textFields![0] as? UITextField{
+                    self.saveItem(field.text!)
+                    self.tableView.reloadData()
+                }
+            
+            }))
+        let cancelAction =  UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler:nil)
+        
+        confirmView.addTextFieldWithConfigurationHandler({
+            (textField) in
+            textField.placeholder = "Insert Item..."
+        })
+        
+        confirmView.addAction(confirmAction)
+        confirmView.addAction(cancelAction)
+        self.presentViewController(confirmView, animated: true, completion: nil)
+    }
+    
+    func saveItem(item: String){
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext
+        let entity = NSEntityDescription.entityForName("ListItems", inManagedObjectContext: managedContext)
+        let item = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+        item.setValue(item, forKey: "item")
+        do{
+            try managedContext.save()
+            listItems.append(item)
+        }catch{
+            print("error")
+        }
+    }
 
 }
 
